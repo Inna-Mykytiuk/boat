7f6dacdd767db4aba1ab75a28c3b97d4e4054ab1
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-06-11 11:47:02 +0300

    Hero Section well done

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
   return (
     <section className="w-full mt-[340px] sm:mt-[280px] md:mt-[260px] mb-[200px] md:mb-[300px] xl:mb-[480px] xxl:mb-[600px]">
       <div className="container">
-        <h3 className="text-dark font-roboto font-bold text-2xl text-center uppercase tracking-tighter">
+        <h3 className="text-dark font-roboto font-bold text-base sm:text-xl md:text-2xl text-center uppercase tracking-tighter">
           Welcome&nbsp;&nbsp;to&nbsp;&nbsp;boat&nbsp;&nbsp;adventure&nbsp;&nbsp;tours
         </h3>
         <Title>Boat</Title>
