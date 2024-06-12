0168329610dbc05e6961c295517897b97cf355b2
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-06-12 14:34:09 +0300

    title part with adaptive well done

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -4,22 +4,22 @@ import Title from "../Title/Title";
 
 export default function Services() {
   return (
-    <section id="services" className="mt-[50px] mb-[50px]">
+    <section id="services" className="mt-[20px] md:mt-[30px] mb-[650px]">
       <div className="container relative">
-        <h2 className="title relative gradient-text font-bold text-[60px] sm:text-[100px] md:text-[180px] xl:text-[250px] font-comforta text-center tracking-tighter leading-tight uppercase">
+        <h2 className="title relative gradient-text font-bold text-[70px] sm:text-[100px] md:text-[180px] xl:text-[250px] font-comforta text-center tracking-tighter leading-tight uppercase">
           Speedy
         </h2>
 
-        <div className="w-[95%] absolute top-[120px] left-[100px] flex justify-center items-center gap-[80px]">
+        <div className=" absolute top-[80px] md:top-[120px] xl:top-[140px] xl:left-[100px] flex flex-col md:flex-row justify-center items-center gap-[20px] md:gap-[40px] xl:gap-[80px]">
           <h3
-            className="relative text-dark font-contrail text-3xl uppercase max-w-[350px]
-          before:absolute before:top-[20px] before:content-[''] before:block before:left-[-80px] before:z-10 before:h-[20px] before:w-[60px] 
+            className="relative text-dark font-contrail text-xl xl:text-3xl uppercase max-w-[280px] md:max-w-[350px] before:hidden text-center
+          md:before:absolute  before:content-[''] md:before:block md:before:left-[100px] md:before:top-[-30px] xl:before:left-[-80px] xl:before:top-[20px] before:z-10 before:h-[20px] before:w-[60px] 
           before:bg-lightBlue before:rounded-full
           "
           >
             Your Premier destination for hight-speed fun
           </h3>
-          <p className="text-dark  font-normal font-comforta text-base  max-w-[600px]">
+          <p className="text-dark  font-normal font-comforta text-base md:max-w-[400px] xl:max-w-[600px] mdOnly:mr-[30px]">
             At boat, we’re passionate about delivering unforgettable experiences
             on the water. We offer safe, thrilling rides that cater to
             adrenaline junkies and sightseers alike. Learn more about our
@@ -27,7 +27,7 @@ export default function Services() {
           </p>
         </div>
 
-        <Card />
+        {/* <Card /> */}
       </div>
     </section>
   );
