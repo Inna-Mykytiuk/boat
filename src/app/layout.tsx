df14a02de2b4ccbc8ef8e60e8285951c8273a2b3
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-06-17 14:41:59 +0300

    add url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import meta from "@/data/meta.json";
 
-const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL as string;
+const NEXT_PUBLIC_URL = "http://localhost:3000/";
 
 const { title, description, manifest, openGraph, icons } = meta;
 
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="!scroll-smooth">
       <body>
         <main className="overflow-hidden flex flex-col min-h-screen">
           {children}
